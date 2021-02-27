012971538b380f649ae511fbc693ca02b2a9096d
khawla-gh <67587914+khawla-gh@users.noreply.github.com>
2021-02-27 17:04:49 +0300

    added colors & appbar to constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 //inputs decorations (for text input feilds)
@@ -39,7 +40,7 @@ final passwordValidation = MultiValidator([
   RequiredValidator(errorText: 'This field is required.'),
   MinLengthValidator(7, errorText: 'Password must be at least 7 characters.'),
   PatternValidator(r'(?=.*?[#?!@$%^&*-])',
-      errorText: 'passwords must have at least one special character')
+      errorText: 'Passwords must have at least one special character')
 ]);
 
 final emailValidation = MultiValidator([
@@ -59,5 +60,105 @@ final phoneValidaton = MultiValidator([
 
 final textAreaValidation = MultiValidator([
   RequiredValidator(errorText: 'This field is required.'),
-  MinLengthValidator(100, errorText: 'Must be at least 100 characters.'),
+  MinLengthValidator(30, errorText: 'Must be at least 30 characters.'),
 ]);
+
+//////////////////////////app theme////////////////////////////////
+
+//colors:
+const primaryBlack = const Color(0xff0B0B0B);
+const secondaryDarkBlue = const Color(0xff14213C);
+const secondaryLightBlue = const Color(0xff23365B);
+const secondaryDarkGrey = const Color(0xffbcbcbc);
+const secondaryLightGrey = const Color(0xffdfdfdf);
+const accentYellow = const Color(0xffF9A21B);
+const accentOrange = const Color(0xffF05824);
+const whiteBG = Colors.white;
+
+// title, back button and  n
+AppBar myAppBar1(
+  context, {
+  String title,
+  IconButton iconButton,
+}) {
+  return AppBar(
+    backgroundColor: secondaryDarkBlue,
+    title: Text(title,
+        style: TextStyle(
+          color: Colors.white,
+        )),
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.white),
+    actions: <Widget>[iconButton],
+    leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        tooltip: 'return to previous screen',
+        onPressed: () {
+          Navigator.pop(context);
+        }),
+  );
+}
+
+// title and back button only
+AppBar myAppBar2(
+  context, {
+  String title,
+}) {
+  return AppBar(
+    backgroundColor: secondaryDarkBlue,
+    title: Text(title,
+        style: TextStyle(
+          color: Colors.white,
+        )),
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.white),
+    leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        tooltip: 'return to previous screen',
+        onPressed: () {
+          Navigator.pop(context);
+        }),
+  );
+}
+
+// with search bar and back button
+AppBar myAppBar3(context, {String title}) {
+  return AppBar(
+    backgroundColor: secondaryDarkBlue,
+    title: Text(title,
+        style: TextStyle(
+          color: Colors.white,
+        )),
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.white),
+    leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        tooltip: 'return to previous screen',
+        onPressed: () {
+          Navigator.pop(context);
+        }),
+    actions: [
+      IconButton(icon: Icon(Icons.search), tooltip: 'search', onPressed: () {}),
+      IconButton(
+          icon: FaIcon(FontAwesomeIcons.filter), iconSize: 20, onPressed: () {})
+    ],
+  );
+}
+
+// search bar withoit back button (for home page)
+AppBar myAppBar4(context, {String title}) {
+  return AppBar(
+    backgroundColor: secondaryDarkBlue,
+    title: Text(title,
+        style: TextStyle(
+          color: Colors.white,
+        )),
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.white),
+    actions: [
+      IconButton(icon: Icon(Icons.search), tooltip: 'search', onPressed: () {}),
+      IconButton(
+          icon: FaIcon(FontAwesomeIcons.filter), iconSize: 20, onPressed: () {})
+    ],
+  );
+}
