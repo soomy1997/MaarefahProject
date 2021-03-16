23e3d817e1f174bd2f21c18a52eaaae206ed5615
khawla-gh <67587914+khawla-gh@users.noreply.github.com>
2021-03-16 17:34:08 +0300

    notification edited

--- lib/notification.dart
+++ lib/notification.dart
@@ -2,33 +2,61 @@ import 'package:flutter/material.dart';
 
 import 'utils/constants.dart';
 
-void main() {
-  runApp(Notification());
-}
-
-class Notification extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Notification',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: NotificationPage(title: 'Notification'),
-    );
-  }
-}
-
 class NotificationPage extends StatefulWidget {
-  NotificationPage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
   _NotificationPage createState() => _NotificationPage();
 }
 
 class _NotificationPage extends State<NotificationPage> {
+  Widget bodyData() => DataTable(
+      columnSpacing: 30,
+      dataRowHeight: 60,
+      dividerThickness: 1,
+      showBottomBorder: true,
+      columns: <DataColumn>[
+        DataColumn(
+          label: Text(
+            ' ',
+          ),
+          numeric: false,
+        ),
+        DataColumn(
+          label: Text(
+            'Notification',
+            style: h4,
+          ),
+          numeric: false,
+        ),
+        DataColumn(
+          label: Text(
+            ' ',
+          ),
+          numeric: false,
+        )
+      ],
+      rows: notificationList
+          .map(
+            (notifications) => DataRow(
+              cells: [
+                DataCell(
+                  Icon(Icons.notifications_active, color: accentYellow),
+                  showEditIcon: false,
+                  placeholder: false,
+                ),
+                DataCell(
+                  Text(notifications.text),
+                  showEditIcon: false,
+                  placeholder: true,
+                ),
+                DataCell(
+                  Icon(Icons.close, color: accentOrange),
+                  showEditIcon: false,
+                  placeholder: false,
+                ),
+              ],
+            ),
+          )
+          .toList());
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: myAppBar2(
@@ -36,24 +64,23 @@ class _NotificationPage extends State<NotificationPage> {
           title: 'Notification',
         ),
         body: Container(
-          child: ListView.builder(
-            itemCount: 20,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                contentPadding: EdgeInsets.fromLTRB(25, 15, 5, 0),
-                title: Text(
-                  'Item $index',
-                  style: h4,
-                ),
-                subtitle: Text('xx minutes ago'),
-                onTap: () {},
-                leading: Icon(
-                  Icons.notifications_active,
-                  color: accentYellow,
-                ),
-              );
-            },
-          ),
+          child: bodyData(),
         ));
   }
 }
+
+class Notificattion {
+  String text;
+
+  Notificattion({this.text});
+}
+
+var notificationList = <Notificattion>[
+  Notificattion(
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+  Notificattion(
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+  Notificattion(
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+];
+//change this to the notification reference from the database
