f9c82700259545d2f6cbf494f1a72d1838c8be69
RawanAalouh <aalouhrawan@gmail.com>
2021-03-28 21:58:56 +0300

    pull the changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_1/add_session.dart';
 import 'package:flutter_app_1/authentication.dart';
+import 'package:flutter_app_1/component/Reviews_card.dart';
+import 'package:flutter_app_1/component/info_card.dart';
 import 'package:flutter_app_1/course_details.dart';
 import 'package:flutter_app_1/edit_account.dart';
 import 'package:flutter_app_1/admin/edit_session_details.dart';
@@ -10,6 +12,8 @@ import 'package:flutter_app_1/no_internet_connection.dart';
 import 'package:flutter_app_1/no_search_found.dart';
 import 'package:flutter_app_1/notification.dart';
 import 'package:flutter_app_1/profile.dart';
+import 'package:flutter_app_1/profile.tutor.dart';
+import 'package:flutter_app_1/profile_visitor.dart';
 import 'package:flutter_app_1/reset.dart';
 import 'package:flutter_app_1/send_email.dart';
 import 'package:flutter_app_1/admin/session_details.dart';
@@ -29,7 +33,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => EditSessionDetails(),
+        '/': (context) => Profile(),
       },
       title: 'Flutter login',
       theme: ThemeData(
