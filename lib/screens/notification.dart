84c387ee6b65142bfaf2ec0a49cc46c62cdb3a12
RawanAalouh <aalouhrawan@gmail.com>
2021-04-29 23:21:27 +0300

    remove comments

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_app_1/services/MessagingWidget.dart';
 
 import '../utils/constants.dart';
 
@@ -34,131 +33,15 @@ class _NotificationPage extends State<NotificationPage> {
         ),
       );
 
-//   Widget notifications() => Column(
-// //today
-//           children: <Widget>[
-//             Container(
-//               padding: const EdgeInsets.only(top: 15),
-//               child: DataTable(
-//                   columnSpacing: 20,
-//                   dataRowHeight: 60,
-//                   dividerThickness: 1,
-//                   showBottomBorder: true,
-//                   columns: <DataColumn>[
-//                     DataColumn(
-//                       label: Text(
-//                         ' ',
-//                         style: h4,
-//                       ),
-//                       numeric: false,
-//                     ),
-//                     DataColumn(
-//                       label: Text(
-//                         'Today',
-//                         style: h4,
-//                       ),
-//                       numeric: false,
-//                     ),
-//                     DataColumn(
-//                       label: Text(
-//                         ' ',
-//                       ),
-//                       numeric: false,
-//                     )
-//                   ],
-//                   rows: notificationList
-//                       .map(
-//                         (notifications) => DataRow(
-//                           cells: [
-//                             DataCell(
-//                               Icon(Icons.notifications_none,
-//                                   color: accentYellow),
-//                               showEditIcon: false,
-//                               placeholder: false,
-//                             ),
-//                             DataCell(
-//                               Text(notifications.text),
-//                               showEditIcon: false,
-//                               placeholder: true,
-//                             ),
-//                             DataCell(
-//                               Icon(Icons.close, color: accentOrange),
-//                               showEditIcon: false,
-//                               placeholder: false,
-//                             ),
-//                           ],
-//                         ),
-//                       )
-//                       .toList()),
-//             ),
-
-// //yesterday
-//             Container(
-//                 padding: const EdgeInsets.only(top: 15),
-//                 child: DataTable(
-//                     columnSpacing: 20,
-//                     dataRowHeight: 60,
-//                     dividerThickness: 1,
-//                     showBottomBorder: false,
-//                     columns: <DataColumn>[
-//                       DataColumn(
-//                         label: Text(
-//                           ' ',
-//                           style: h4,
-//                         ),
-//                         numeric: false,
-//                       ),
-//                       DataColumn(
-//                         label: Text(
-//                           'Yesterday',
-//                           style: h4,
-//                         ),
-//                         numeric: false,
-//                       ),
-//                       DataColumn(
-//                         label: Text(
-//                           ' ',
-//                         ),
-//                         numeric: false,
-//                       )
-//                     ],
-//                     rows: notificationList
-//                         .map(
-//                           (notifications) => DataRow(
-//                             cells: [
-//                               DataCell(
-//                                 Icon(Icons.notifications_none,
-//                                     color: accentYellow),
-//                                 showEditIcon: false,
-//                                 placeholder: false,
-//                               ),
-//                               DataCell(
-//                                 Text(notifications.text),
-//                                 showEditIcon: false,
-//                                 placeholder: true,
-//                               ),
-//                               DataCell(
-//                                 Icon(Icons.close, color: accentOrange),
-//                                 showEditIcon: false,
-//                                 placeholder: false,
-//                               ),
-//                             ],
-//                           ),
-//                         )
-//                         .toList()))
-//           ]);
-
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: myAppBar4(
           context,
           title: 'Notifications',
         ),
-        body: Container(child:
-        Text('data')
-        //  MessagingWidget()
-         )
-         );
+        body: Container(child: Text('data')
+            //  MessagingWidget()
+            ));
 
     /*Container(
           child: notificationList.isEmpty ? messageWidget() : notifications(),
